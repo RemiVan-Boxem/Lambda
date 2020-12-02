1513b81c7f36bef5f228ba8061bdffedc81db975
Rémi VAN BOXEM <71462457+RemiVan-Boxem@users.noreply.github.com>
2020-12-02 14:27:22 +0100

    Update Lambda.py

--- Lambda.py
+++ Lambda.py
@@ -1,9 +1,7 @@
 class LambdaTerm():
     "A Classic Lambda Term"
 
-    def __init__(self, atom, functions = 0):
-        self.atom = atom
-        self.functions = functions
-
-    class Atom(LambdaTerm):
-        ?
+class Atom(LambdaTerm):
+    "A Simple Atom"
+    def __init__(self, value):
+        self.value = value
