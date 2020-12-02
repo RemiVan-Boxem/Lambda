0315d63747d4a92f33651c4e6288046edd0de2b3
Rémi VAN BOXEM <71462457+RemiVan-Boxem@users.noreply.github.com>
2020-12-02 22:40:31 +0100

    Adding abstraction

--- Lambda.py
+++ Lambda.py
@@ -5,3 +5,9 @@ class Atom(LambdaTerm):
     "A Simple Atom"
     def __init__(self, value):
         self.value = value
+        
+class Abstraction(LambdaTerm):
+    "An Abstraction, QED"
+    def __init__(self, name, body):
+        self.name = name
+        self.body = body
