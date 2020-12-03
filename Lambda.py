5b896853341cff35bf22522f32da4e407de8e3cb
Rémi VAN BOXEM <71462457+RemiVan-Boxem@users.noreply.github.com>
2020-12-03 21:51:54 +0100

    Adding applications

--- Lambda.py
+++ Lambda.py
@@ -11,3 +11,9 @@ class Abstraction(LambdaTerm):
     def __init__(self, name, body):
         self.name = name
         self.body = body
+
+        class Application(LambdaTerm):
+    "An Application thing"
+    def __init__(self, lta, ltb): #ltX : LambdaTerm X
+        self.lambdaterma : LambdaTerm = lta
+        self.lambdatermb : LambdaTerm = ltb 
